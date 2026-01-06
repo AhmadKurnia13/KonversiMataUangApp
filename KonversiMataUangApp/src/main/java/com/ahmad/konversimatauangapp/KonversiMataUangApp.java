8ef823acc4cff13bef79969267ddc854bf695132
Ahmad Kurnia <ahmadkurnia779@gmail.com>
2026-01-06 20:10:42 +0700

    Add files via upload

---
+++ KonversiMataUangApp/src/main/java/com/ahmad/konversimatauangapp/KonversiMataUangApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ahmad.konversimatauangapp;
+
+/**
+ *
+ * @author MyBook Hype AMD
+ */
+public class KonversiMataUangApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
